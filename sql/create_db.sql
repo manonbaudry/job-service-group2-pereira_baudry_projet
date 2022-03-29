5a6692262291b5b9bc350ce2f3f8b3ef3be7a5f6
manonbaudry <man.baudry@gmail.com>
2022-03-29 13:49:02 +0000

    add script

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -14,3 +14,7 @@ CREATE TABLE "Job"
     is_deleted BOOLEAN,
     ranking REAL
 );
+
+INSERT INTO "Job" (id, title, company, city, job_description, company_description, created_at, end_date, contact_email, contract_type, duration, is_deleted) 
+VALUES  ("9241a0d5-2732-4331-a636-0dc195502c1d", "title", "company", "lille","job description", 
+"company description", "29/03/2022", "30/03/2022", "qsi@cfini.fr", "CDI", "2 ans", FALSE);
